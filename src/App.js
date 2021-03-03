8be3eea3c20d892922bff00d72a616df581f987e
SVitya <s.vitya96@gmail.com>
2021-03-03 17:42:17 +0200

    Add fetching from API
    Add displaying current position and weather

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [location, setLocation] = useState({});
+  const [weather, setWeather] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      let {latitude, longitude} = pos.coords;
+      axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${process.env.REACT_APP_GEOCODE_API_KEY}&language=uk&result_type=locality`)
+        .then(res => setLocation({
+          name: res.data.results[0].address_components[0].long_name,
+          lat: latitude,
+          lon: longitude
+        }))
+    });  
+  }, []);
+
+  useEffect(() => {
+    if (Object.keys(location).length !== 0) {
+      axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${location.lat}&lon=${location.lon}&exclude=minutely,hourly,alerts&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`)
+        .then(res => {setWeather(res.data)})
+    }
+  }, [location]);
+
   return (
     <div>
-      App works
+      {location.name}
+      {weather.current.temp}
     </div>
   );
 }
